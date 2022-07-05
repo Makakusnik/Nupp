8c86f5030cb99ed00b0c00560ac7a29f05f784ac
Marek Fodor <fodormarek5@gmail.com>
2022-07-05 14:52:52 +0200

    Set new fonts for headings & body

--- theme/index.ts
+++ theme/index.ts
@@ -1,5 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/lato";
 
-const theme = extendTheme({});
+const theme = extendTheme({
+  fonts: {
+    heading: `'Lato', 'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+});
 
 export default theme;
