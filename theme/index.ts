3b8acd4cda194c5775d6e3a576f619980a362eed
Marek Fodor <fodormarek5@gmail.com>
2022-07-06 11:23:20 +0200

    Adjusted config

--- theme/index.ts
+++ theme/index.ts
@@ -1,7 +1,13 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 import "@fontsource/lato";
 
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
 const theme = extendTheme({
+  config: config,
   fonts: {
     heading: `'Lato', 'Inter', sans-serif`,
     body: `'Inter', sans-serif`,
