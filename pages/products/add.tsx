bd21f5cf70108e6d0da22e3cc99588c9a833472b
Marek Fodor <fodormarek5@gmail.com>
2022-07-18 14:57:07 +0200

    Started building form

--- pages/products/add.tsx
+++ pages/products/add.tsx
@@ -1,33 +1,26 @@
 import {
   Box,
-  Button,
-  ButtonGroup,
-  Center,
   Container,
-  Heading,
+  FormControl,
+  FormHelperText,
+  FormLabel,
   HStack,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  Stack,
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
-import CategoriesContainer from "../../components/Content/Categories";
-import Header, {
-  HeaderHeading,
-  HeaderImageSection,
-  HeaderMainSection,
-  HeaderText,
-  SmallHeader,
-} from "../../components/Content/Header";
+import * as Header from "../../components/Content/Header";
 import { GiMeal } from "react-icons/gi";
 import { FaAppleAlt } from "react-icons/fa";
-import { DatabaseTable } from "../../components/Database/DatabaseTable";
-import { FilterBox } from "../../components/Database/Filters";
-import { BlobAnimatedJsx } from "../../components/Misc/BlobVector";
-import { dataBlue } from "../../testdata/blobData";
-import { IconType } from "react-icons";
 import { useEffect, useState } from "react";
 import { BigButton } from "../../components/Input";
+import { MainHeading } from "../../components/Content/Form/Header";
 
 const Products: NextPage = () => {
   const [isMealForm, setIsMealForm] = useState(false);
@@ -61,8 +54,18 @@ const Products: NextPage = () => {
           productButtonHandler={handleProductButton}
         />
       )}
-      {isMealForm && !isChoosingState && <MealForm />}
-      {isProductForm && !isChoosingState && <ProductForm />}
+      {!isChoosingState && (
+        <Container
+          px="32px"
+          maxW="container.lg"
+          display="flex"
+          flexDirection={"column"}
+          alignItems={"center"}
+        >
+          {isMealForm && <MealForm />}
+          {isProductForm && <ProductForm />}
+        </Container>
+      )}
     </Container>
   );
 };
@@ -77,15 +80,15 @@ interface IntroProps {
 const Intro = ({ mealButtonHandler, productButtonHandler }: IntroProps) => {
   return (
     <>
-      <SmallHeader h={{ base: "300px" }} isTransparent={true}>
-        <HeaderMainSection>
-          <HeaderHeading>Add Product or Meal</HeaderHeading>
-          <HeaderText>
+      <Header.SmallHeader h={{ base: "300px" }} isTransparent={true}>
+        <Header.MainSection>
+          <Header.Title>Add Product or Meal</Header.Title>
+          <Header.Description>
             You can add your meal or product to our database! Don't worry if you
             don't have all informations, we will supply them later.
-          </HeaderText>
-        </HeaderMainSection>
-      </SmallHeader>
+          </Header.Description>
+        </Header.MainSection>
+      </Header.SmallHeader>
       <Container maxW="container.lg" marginTop={"40px"}>
         <HStack margin="auto" spacing="40px" justifyContent={"center"}>
           <BigButton
@@ -111,18 +114,18 @@ const Intro = ({ mealButtonHandler, productButtonHandler }: IntroProps) => {
 const MealForm = () => {
   return (
     <>
-      <SmallHeader h={{ base: "300px" }} isTransparent={true}>
-        <HeaderMainSection>
-          <HeaderHeading>Add Product or Meal</HeaderHeading>
-          <HeaderText>
+      <Header.SmallHeader h={{ base: "300px" }} isTransparent={true}>
+        <Header.MainSection>
+          <Header.Title>Add Product or Meal</Header.Title>
+          <Header.Description>
             You can add your meal or product to our database! Don't worry if you
             don't have all informations, we will supply them later.
-          </HeaderText>
-        </HeaderMainSection>
-        <HeaderImageSection>
+          </Header.Description>
+        </Header.MainSection>
+        <Header.ImageSection>
           <Box as={FaAppleAlt} size="128px"></Box>
-        </HeaderImageSection>
-      </SmallHeader>
+        </Header.ImageSection>
+      </Header.SmallHeader>
     </>
   );
 };
@@ -130,14 +133,91 @@ const MealForm = () => {
 const ProductForm = () => {
   return (
     <>
-      <SmallHeader h={{ base: "300px" }} isTransparent={true}>
-        <HeaderMainSection>
-          <HeaderHeading>Product</HeaderHeading>
-        </HeaderMainSection>
-        <HeaderImageSection>
+      <Header.SmallHeader h={{ base: "300px" }} isTransparent={true}>
+        <Header.MainSection>
+          <Header.Title>Product</Header.Title>
+        </Header.MainSection>
+        <Header.ImageSection placeContent="center center">
           <Box as={FaAppleAlt} size="128px"></Box>
-        </HeaderImageSection>
-      </SmallHeader>
+        </Header.ImageSection>
+      </Header.SmallHeader>
+      <FormControl
+        w="container.md"
+        display="flex"
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <MainHeading>Product Details</MainHeading>
+        <HStack marginTop="24px" alignItems={"start"} justifyContent="start">
+          <VStack w="60%" px="16px" spacing="24px">
+            <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+              <FormLabel htmlFor="ProductName" whiteSpace={"nowrap"}>
+                Product Name
+              </FormLabel>
+              <Input w="25ch" id="ProductName" type="text" />
+            </HStack>
+            <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+              <FormLabel htmlFor="BrandName" whiteSpace={"nowrap"}>
+                Brand Name
+              </FormLabel>
+              <Input id="BrandName" w="25ch" />
+            </HStack>
+            <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+              <FormLabel htmlFor="PackingWeight" whiteSpace={"nowrap"}>
+                Packing Weight
+              </FormLabel>
+              <InputGroup w="25ch">
+                <Input id="PackingWeight" w="25ch" type="number" />
+                <InputRightAddon>grams</InputRightAddon>
+              </InputGroup>
+            </HStack>
+            <VStack w="100%" alignItems={"end"}>
+              <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+                <FormLabel htmlFor="VendorPrice" whiteSpace={"nowrap"}>
+                  Vendor {"&"} Price
+                </FormLabel>
+                <HStack w="25ch">
+                  <Input id="VendorName" />
+                  <InputGroup w="10ch">
+                    <Input px="1" type={"number"} w="6ch" id="VendorPrice" />
+                    <InputRightAddon p="3">$</InputRightAddon>
+                  </InputGroup>
+                </HStack>
+              </HStack>
+            </VStack>
+            <VStack w="100%" alignItems={"end"}>
+              <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+                <FormLabel htmlFor="VendorPrice" whiteSpace={"nowrap"}>
+                  Food Additives
+                </FormLabel>
+                <Select id="BrandName" w="25ch" />
+              </HStack>
+            </VStack>
+            <VStack w="100%" alignItems={"end"}>
+              <HStack w="100%" spacing="24px" justifyContent={"space-between"}>
+                <FormLabel htmlFor="VendorPrice" whiteSpace={"nowrap"}>
+                  Alergens
+                </FormLabel>
+                <Select id="BrandName" w="25ch" />
+              </HStack>
+            </VStack>
+          </VStack>
+
+          <VStack w="40%">
+            <Box h="256px" w="256px" bg="red"></Box>
+          </VStack>
+        </HStack>
+        <VStack marginTop="32px" alignContent="center" w="container.sm">
+          <MainHeading>Product Details</MainHeading>
+          <Box h="256px" w="256px" bg="red"></Box>
+        </VStack>
+      </FormControl>
     </>
   );
 };
+
+type FormFieldProps = {
+  id: string;
+};
+
+const FormInput = () => {};
