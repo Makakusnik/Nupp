5307d8e28d22cc7ed2ae2858d52ec4c33dbb03eb
Marek Fodor <fodormarek5@gmail.com>
2022-07-26 13:18:40 +0200

    Added some shared functions amongst Content

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,1 +1,22 @@
+import { SyntheticEvent } from "react";
+
+export { MealForm } from "./Pages/products/add/mealAdd";
 export { ProductForm } from "./Pages/products/add/productAdd";
+
+export const handleSimpleTextInput = (
+  e: SyntheticEvent<HTMLInputElement>,
+  setter: ((value: string) => void) | null
+) => {
+  if (setter) setter(e.currentTarget.value);
+};
+
+export const handleSimpleNumberInput = (
+  e: SyntheticEvent<HTMLInputElement>,
+  setter: ((value: number) => void) | null
+) => {
+  if (setter) setter(Number(e.currentTarget.value));
+};
+
+export type SimpleFieldType<T> = {
+  setter: ((value: T) => void) | null;
+};
