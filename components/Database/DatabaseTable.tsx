2798193aee8f33e7474bd626103fff3b5f518e72
Marek Fodor <fodormarek5@gmail.com>
2022-07-26 13:21:46 +0200

    Added options to data table + added small variant

--- components/Database/DatabaseTable.tsx
+++ components/Database/DatabaseTable.tsx
@@ -9,29 +9,54 @@ import {
   TableContainer,
   Box,
   IconButton,
+  Container,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  HStack,
+  useColorModeValue,
 } from "@chakra-ui/react";
-import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { SyntheticEvent, useState } from "react";
+import { MdAdd, MdCheck, MdOutlineFavoriteBorder, MdPlusOne } from "react-icons/md";
 import { data, Data } from "../../testdata/data";
+import { ChildrenOnlyProps } from "../../types/Types";
 import { DataTableLink } from "../Navigation";
 
 const formatSecondaryData = (data: string) => {
   return <DataTableLink href="#">{data}</DataTableLink>;
 };
 
-export const DatabaseTable = () => {
+type DataTableProps = {
+  hasEnergy?: boolean;
+  hasFats?: boolean;
+  hasCarbohydrates?: boolean;
+  hasProteins?: boolean;
+  hasSalt?: boolean;
+  hasAvgPrice?: boolean;
+};
+
+export const DatabaseTable = ({
+  hasEnergy = true,
+  hasFats = true,
+  hasCarbohydrates = true,
+  hasProteins = true,
+  hasSalt = true,
+  hasAvgPrice = true,
+}: DataTableProps) => {
   return (
-    <TableContainer marginTop='40px'>
+    <TableContainer marginTop="40px">
       <Table variant="simple">
         <TableCaption>Grocery database from Slovak grocery shops</TableCaption>
         <Thead>
           <Tr>
             <TableHead title="Name" details="Brand / Recipe Link" />
-            <TableHead title="Energy" details="g / 100 g" align={true} />
-            <TableHead title="Fats" details="g / 100 g" align={true} />
-            <TableHead title="Carbs" details="g / 100 g" align={true} />
-            <TableHead title="Proteins" details="g / 100 g" align={true} />
-            <TableHead title="Salt" details="g / 100 g" align={true} />
-            <TableHead title="Avg. Price" details="$" align={true} />
+            {hasEnergy && <TableHead title="Energy" details="g / 100 g" alignCenter={true} />}
+            {hasFats && <TableHead title="Fats" details="g / 100 g" alignCenter={true} />}
+            {hasCarbohydrates && <TableHead title="Carbohydrates" details="g / 100 g" alignCenter={true} />}
+            {hasProteins && <TableHead title="Proteins" details="g / 100 g" alignCenter={true} />}
+            {hasSalt && <TableHead title="Salt" details="g / 100 g" alignCenter={true} />}
+            {hasAvgPrice && <TableHead title="Avg. Price" details="$" alignCenter={true} />}
           </Tr>
         </Thead>
         <Tbody>
@@ -54,11 +79,7 @@ const FormatedTableRow = ({ item }: TableRowProps) => {
       <Td>
         {item.name}
 
-        <IconButton
-          marginLeft="8px"
-          size="xs"
-          aria-label="Add Product To Favorits"
-        >
+        <IconButton marginLeft="8px" size="xs" aria-label="Add Product To Favorits">
           <Box as={MdOutlineFavoriteBorder}></Box>
         </IconButton>
         <br></br>
@@ -76,16 +97,92 @@ const FormatedTableRow = ({ item }: TableRowProps) => {
 
 interface TableHeadProps {
   title: string;
-  details: string;
-  align?: boolean;
+  details?: string;
+  alignCenter?: boolean;
 }
 
-const TableHead = ({ title, details, align }: TableHeadProps) => {
+const TableHead = ({ title, details, alignCenter }: TableHeadProps) => {
   return (
-    <Th textAlign={align ? "center" : "left"}>
+    <Th textAlign={alignCenter ? "center" : "left"}>
       {title}
       <br></br>
       <span style={{ color: "gray", fontSize: "10px" }}>{details}</span>
     </Th>
+  );
+};
+
+export const SmallDataTable = ({ children }: ChildrenOnlyProps) => {
+  const bg = useColorModeValue("white", "gray.800");
+  return (
+    <TableContainer w="100%">
+      <Box overflowY="auto" overflowX="hidden" maxHeight="500px">
+        <Table variant="striped" size="sm">
+          <Thead bg={bg} zIndex="2" position="sticky" top={0}>
+            <Tr>
+              <TableHead title="Name" details="Brand / Recipe Link" />
+              <TableHead title="Add" alignCenter={true} />
+            </Tr>
+          </Thead>
+          <Tbody w="100%" minW="100%">
+            {children}
+          </Tbody>
+        </Table>
+      </Box>
+    </TableContainer>
+  );
+};
+
+type SmallTableRowProps = {
+  ariaLabelIconButton: string;
+};
+
+export const SmallTableRow = ({ item, ariaLabelIconButton }: TableRowProps & SmallTableRowProps) => {
+  const [isFieldActive, setActiveField] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setActiveField(true);
+  };
+
+  const handleBlur = () => {
+    setActiveField(false);
+  };
+  return (
+    <Tr>
+      <Td w="50%">
+        {item.name}
+        {formatSecondaryData(item.secondaryData)}
+      </Td>
+      <Td w="50%" justifyContent="center">
+        <HStack spacing="8px" justifyContent="center">
+          {isFieldActive ? (
+            <>
+              <InputGroup size="xs">
+                <Input type="number" w="100%" onBlur={handleBlur} />
+                <InputRightAddon>g</InputRightAddon>
+              </InputGroup>
+              <IconButton
+                colorScheme="green"
+                marginLeft="8px"
+                size="xs"
+                onClick={handleClick}
+                aria-label={ariaLabelIconButton}
+              >
+                <Box size="16px" as={MdCheck}></Box>
+              </IconButton>
+            </>
+          ) : (
+            <IconButton
+              colorScheme="green"
+              marginLeft="8px"
+              size="xs"
+              onClick={handleClick}
+              aria-label={ariaLabelIconButton}
+            >
+              <Box size="16px" as={MdAdd}></Box>
+            </IconButton>
+          )}
+        </HStack>
+      </Td>
+    </Tr>
   );
 };
