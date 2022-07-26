1f0b37cd5b755ab43074bf7213d601eeb8290783
Marek Fodor <fodormarek5@gmail.com>
2022-07-26 13:22:29 +0200

    Small Fixes, added Form to meal add page

--- pages/products/add/meal.tsx
+++ pages/products/add/meal.tsx
@@ -1,32 +1,36 @@
 import { Box, Container } from "@chakra-ui/react";
 import * as Header from "../../../components/Content/Header";
-import { FaAppleAlt } from "react-icons/fa";
+import { GiMeal } from "react-icons/gi";
 import Head from "next/head";
 import { NextPage } from "next";
+import { MealFormContextProvider } from "../../../components/Contexts/MealFormContext";
+import { MealForm } from "../../../components/Content";
+import { PageContainer } from "../../../components/Misc/Layout";
 
 const AddMeal: NextPage = () => {
   return (
-    <Container p="0" paddingTop={"88px"} w="100%" minW="100%">
+    <PageContainer>
       <Head>
-        <title>Nupp | Add Item Form</title>
-        <meta name="description" content="You can create and add product to our database." />
+        <title>Nupp | Add Meal Form</title>
+        <meta name="description" content="You can create and add meal to our database." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container px="32px" w="container.lg" minW="container.lg" maxW="container.lg">
         <Header.SmallHeader h={{ base: "300px" }} isTransparent={true}>
           <Header.MainSection>
-            <Header.Title>Add Product or Meal</Header.Title>
-            <Header.Description>
-              You can add your meal or product to our database! Don't worry if you don't have all informations, we will
-              supply them later.
-            </Header.Description>
+            <Header.Title>Meal</Header.Title>
           </Header.MainSection>
-          <Header.ImageSection>
-            <Box as={FaAppleAlt} size="128px"></Box>
+          <Header.ImageSection placeContent="center center">
+            <Box as={GiMeal} size="128px"></Box>
           </Header.ImageSection>
         </Header.SmallHeader>
       </Container>
-    </Container>
+      <Container maxW="container.md">
+        <MealFormContextProvider>
+          <MealForm />
+        </MealFormContextProvider>
+      </Container>
+    </PageContainer>
   );
 };
 export default AddMeal;
