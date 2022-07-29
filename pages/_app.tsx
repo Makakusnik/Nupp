4037648e14ebfa61573f1bc8b481ace24b7cb644
Marek Fodor <fodormarek5@gmail.com>
2022-07-29 09:08:43 +0200

    Added CSS reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../theme";
 import { NavigationBar } from "../components/Navigation";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <CSSReset />
       <NavigationBar />
       <Component {...pageProps} />
     </ChakraProvider>
