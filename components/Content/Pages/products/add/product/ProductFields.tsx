948e6e81556e1d0a28be69aa291712f3dee5bcb1
Marek Fodor <fodormarek5@gmail.com>
2022-08-01 09:59:40 +0200

    Added handlers + Mark component

--- components/Content/Pages/products/add/product/ProductFields.tsx
+++ components/Content/Pages/products/add/product/ProductFields.tsx
@@ -1,12 +1,45 @@
-import { FormLabel, InputGroup, Input, InputRightAddon, IconButton, Box, Text, HStack, Spacer } from "@chakra-ui/react";
+import {
+  FormLabel,
+  InputGroup,
+  Input,
+  InputRightAddon,
+  Box,
+  Text,
+  Spacer,
+  Menu,
+  Button,
+  MenuButton,
+  MenuList,
+  Icon,
+  Flex,
+  IconButton,
+  VisuallyHidden,
+  Container,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useState, useRef, SyntheticEvent, useEffect } from "react";
-import { MdAdd, MdClose } from "react-icons/md";
-import { foodAdditives, FoodAdditive, Alergen, Alergens as alergens } from "../../../../../../testdata/data";
+import { MdClose, MdExpandMore } from "react-icons/md";
+import {
+  foodAdditives,
+  FoodAdditive,
+  Alergen,
+  Alergens as alergens,
+  VendorData,
+  MarkType,
+} from "../../../../../../testdata/data";
 import { Select } from "../../../../../Input";
-import { CustomTooltip } from "../../../../../Misc/Tooltip";
+import { CustomTooltip, IconTooltip } from "../../../../../Misc/Tooltip";
 import * as FormElements from "../../../../../Input/Form/FormElements";
-import { VendorPricePairType } from "../../../../../../types/Types";
-import { SimpleFieldType } from "../../../..";
+import { AddButton, RemoveButton } from "../../../../../Input/Buttons/ActionButtons";
+import { ListItemProps } from "../../../../../../types/Types";
+import { CustomIcon, getIcon } from "../../../../../Custom Icons/Icons";
+import { MarkMenuItem } from "../../../../../Input/Form/MenuItem";
+
+export type VendorPricePairType = {
+  id: string;
+  name: string;
+  price: number;
+};
 
 type VendorPriceFieldProps = {
   setter: ((value: VendorPricePairType[]) => void) | null;
@@ -47,7 +80,14 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
     if (inputRef.current) inputRef.current.value = Number(inputRef.current?.value).toFixed(2);
     setter &&
       values &&
-      setter([{ name: selectRef.current?.value || "", price: Number(inputRef.current?.value) || 0 }, ...values]);
+      setter([
+        {
+          name: selectRef.current?.value || "",
+          price: Number(inputRef.current?.value) || 0,
+          id: VendorData.filter((item) => item.name === selectRef.current?.value)[0].id,
+        },
+        ...values,
+      ]);
     if (selectRef.current) {
       selectRef.current.selectedIndex = 0;
     }
@@ -56,12 +96,16 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
     }
     setButtonDisabled(true);
   };
-  useEffect(() => {}, []);
+
+  const handleRemove = (id: string) => {
+    console.log(values.filter((item) => item.id === id)[0]);
+    //if (false) setter && values && setter([values.filter((item) => item.id === id)[0], ...values]);
+  };
 
   return (
     <FormElements.Wrapper isRequired={false}>
       <FormElements.LabelSection>
-        <FormLabel htmlFor="foodAdditives" whiteSpace={"nowrap"}>
+        <FormLabel htmlFor="vendorAndPrice" whiteSpace={"nowrap"}>
           Vendor {"&"} Price
         </FormLabel>
       </FormElements.LabelSection>
@@ -69,9 +113,9 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
       <FormElements.MainSection>
         <FormElements.InputSection>
           <Select
-            id={"vendorPrice"}
+            id={"vendorAndPrice"}
             placeholder={"Pick Vendor"}
-            data={foodAdditives}
+            data={VendorData}
             onChange={isValid}
             ref={selectRef}
           />
@@ -88,14 +132,7 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
             />
             <InputRightAddon>$</InputRightAddon>
           </InputGroup>
-          <IconButton
-            isDisabled={isButtonDisabled}
-            onClick={handleAdd}
-            aria-label="Add Vendor & Price pair."
-            icon={<MdAdd size="24px" />}
-            colorScheme="green"
-            size="xs"
-          />
+          <AddButton onClick={handleAdd} isDisabled={isButtonDisabled} ariaLabel="Add Vendor & Price Pair" />
         </FormElements.InputSection>
         <FormElements.ContainerSection>
           {values &&
@@ -103,7 +140,12 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
             values.map((item, index) => {
               return (
                 <FormElements.SelectedItem key={index}>
-                  <VendorPricePair name={item.name} price={item.price}></VendorPricePair>
+                  <VendorPriceItem
+                    name={item.name}
+                    id={item.id}
+                    onClick={handleRemove}
+                    price={item.price}
+                  ></VendorPriceItem>
                 </FormElements.SelectedItem>
               );
             })}
@@ -113,12 +155,12 @@ export const VendorAndPriceComponent = ({ setter, values }: VendorPriceFieldProp
   );
 };
 
-type VendorPricePair = {
+type VendorPricePairProps = {
   name: string;
   price: number;
-};
+} & ListItemProps;
 
-const VendorPricePair = ({ name, price }: { name: string; price: number }) => {
+const VendorPriceItem = ({ name, price, id, onClick }: VendorPricePairProps) => {
   return (
     <>
       <Box minW="8px" minH="8px" bg={"gray.300"} />
@@ -128,35 +170,7 @@ const VendorPricePair = ({ name, price }: { name: string; price: number }) => {
       <Text textAlign="end" minW="6ch" maxW="6ch" noOfLines={1}>
         {price + " "} $
       </Text>
-      <IconButton aria-label="Remove Option" colorScheme="red" size="xs" icon={<MdClose size="24px" />}></IconButton>
-    </>
-  );
-};
-
-const FoodAdditiveItem = ({ type, code, id, name }: FoodAdditive) => {
-  const [color, setColor] = useState("gray.900");
-  useEffect(() => {
-    switch (type) {
-      case "dangerous":
-        setColor("red.500");
-        break;
-      case "healthy":
-        setColor("green.500");
-        break;
-      case "neutral":
-        setColor("yellow.400");
-        break;
-    }
-  }, []);
-
-  return (
-    <>
-      <Box w="8px" h="8px" bg={color} />
-      <Text w="4ch">{code}</Text>
-      <Text minW="18ch" maxW="18ch" noOfLines={1}>
-        {name}
-      </Text>
-      <IconButton aria-label="Remove Option" colorScheme="red" size="xs" icon={<MdClose size="24px" />}></IconButton>
+      <RemoveButton ariaLabel="Remove Vendor & Price pair" onClick={() => onClick(id)} />
     </>
   );
 };
@@ -167,6 +181,11 @@ type FoodAdditivesComponentProps = {
 };
 
 export const FoodAdditivesComponent = ({ values, setter }: FoodAdditivesComponentProps) => {
+  /*
+    Adds whole FoodAdditive object to values
+    IF - object isn't already there
+    IF - Selected index is not equal to 0
+  */
   const handleSelect = (e: SyntheticEvent<HTMLSelectElement>) => {
     e.preventDefault();
     const value = e.currentTarget.value;
@@ -178,6 +197,11 @@ export const FoodAdditivesComponent = ({ values, setter }: FoodAdditivesComponen
     e.currentTarget.selectedIndex = 0;
 
     setter && values && setter([foodAdditives.filter((item) => item.name === value)[0], ...values]);
+  };
+
+  const handleRemove = (id: string) => {
+    console.log(values.filter((item) => item.id === id)[0]);
+    //if (false) setter && values && setter([values.filter((item) => item.id === id)[0], ...values]);
   };
   return (
     <FormElements.Wrapper isRequired={false}>
@@ -193,7 +217,13 @@ export const FoodAdditivesComponent = ({ values, setter }: FoodAdditivesComponen
             values.map((item) => {
               return (
                 <FormElements.SelectedItem key={item.id}>
-                  <FoodAdditiveItem code={item.code} name={item.name} type={item.type} id={item.id}></FoodAdditiveItem>
+                  <FoodAdditiveItem
+                    code={item.code}
+                    name={item.name}
+                    type={item.type}
+                    onClick={handleRemove}
+                    id={item.id}
+                  />
                 </FormElements.SelectedItem>
               );
             })}
@@ -203,12 +233,51 @@ export const FoodAdditivesComponent = ({ values, setter }: FoodAdditivesComponen
   );
 };
 
+type FoodAdditiveItemProps = {
+  type: "healthy" | "warning" | "dangerous";
+  code: string;
+} & ListItemProps;
+
+const FoodAdditiveItem = ({ type, code, name, onClick, id }: FoodAdditiveItemProps) => {
+  const [color, setColor] = useState("gray.900");
+  useEffect(() => {
+    switch (type) {
+      case "dangerous":
+        setColor("red.500");
+        break;
+      case "healthy":
+        setColor("green.500");
+        break;
+      case "warning":
+        setColor("yellow.400");
+        break;
+    }
+  }, [type]);
+
+  return (
+    <>
+      <Box w="8px" h="8px" bg={color} />
+      <Text w="4ch">{code}</Text>
+      <Text minW="18ch" maxW="18ch" noOfLines={1}>
+        {name}
+      </Text>
+      <RemoveButton ariaLabel="Remove Food Additive" onClick={() => onClick(id)} />
+    </>
+  );
+};
+
 type AlergensComponentProps = {
   values: Alergen[];
   setter: ((values: Alergen[]) => void) | null;
 };
 
 export const AlergensComponent = ({ values, setter }: AlergensComponentProps) => {
+  /*
+    Adds whole Alergen object to values
+    IF - object isn't already there
+    &&
+    IF - Selected index is not equal to 0
+  */
   const handleSelect = (e: SyntheticEvent<HTMLSelectElement>) => {
     e.preventDefault();
     const value = e.currentTarget.value;
@@ -220,6 +289,10 @@ export const AlergensComponent = ({ values, setter }: AlergensComponentProps) =>
     e.currentTarget.selectedIndex = 0;
 
     setter && values && setter([alergens.filter((item) => item.name === value)[0], ...values]);
+  };
+
+  const handleRemove = (id: string) => {
+    console.log(values.filter((item) => item.id === id)[0]);
   };
   return (
     <FormElements.Wrapper isRequired={false}>
@@ -235,7 +308,7 @@ export const AlergensComponent = ({ values, setter }: AlergensComponentProps) =>
             values.map((item) => {
               return (
                 <FormElements.SelectedItem key={item.id}>
-                  <AlergenItem name={item.name}></AlergenItem>
+                  <AlergenItem name={item.name} id={item.id} onClick={handleRemove}></AlergenItem>
                 </FormElements.SelectedItem>
               );
             })}
@@ -245,20 +318,231 @@ export const AlergensComponent = ({ values, setter }: AlergensComponentProps) =>
   );
 };
 
-type AlergenItemProps = {
-  name: string;
-};
-
-export const AlergenItem = ({ name }: AlergenItemProps) => {
+export const AlergenItem = ({ name, onClick, id }: ListItemProps) => {
   return (
-    <HStack as="li" w="100%">
+    <>
       <Box w="8px" h="8px" bg={"red.500"} />
       <Text minW="18ch" maxW="18ch" noOfLines={1}>
         {name}
       </Text>
       <Spacer />
-      <IconButton aria-label="Remove Option" colorScheme="red" size="xs" icon={<MdClose size="24px" />}></IconButton>
-    </HStack>
+      <RemoveButton ariaLabel={"Remove Alergen"} onClick={() => onClick(id)} />
+    </>
+  );
+};
+
+type MarkComponentProps = {
+  values: MarkType[];
+  setter: ((values: MarkType[]) => void) | null;
+};
+
+export const MarksComponent = ({ values, setter }: MarkComponentProps) => {
+  const handleSelect = (
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string,
+    name: string,
+    iconName: CustomIcon,
+    type: "healthy" | "warning" | "dangerous"
+  ) => {
+    if (values.some((item) => item.id === id)) {
+      return;
+    }
+    setter && setter([{ name: name, id: id, iconName: iconName, type: type }, ...values]);
+  };
+
+  const handleRemoveClick = (e: SyntheticEvent, id: string) => {
+    console.log(values.filter((item) => item.id === id)[0]);
+  };
+
+  return (
+    <Menu>
+      <MenuButton w="100%" as={Button} rightIcon={<MdExpandMore size="20px" />}>
+        Pick Marks
+      </MenuButton>
+      <MenuList minW="100%" maxH="360px" overflowY="auto">
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Vitamin"} name={"Vitamins"} id={"125673"}>
+          Has Lot of Vitamins
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"warning"}
+          iconName={"Additives"}
+          name={"Additives"}
+          id={"12673"}
+        >
+          Has Additives
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"dangerous"}
+          iconName={"Additives"}
+          name={"Dangerous Additives"}
+          id={"1723"}
+        >
+          Has Dangerous Additives
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Fiber"} name={"Fiber"} id={"1203"}>
+          Has Fiber
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Minerals"} name={"Minerals"} id={"1923"}>
+          Has Minerals
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"healthy"}
+          iconName={"NoAdditives"}
+          name={"No Additives"}
+          id={"16723"}
+        >
+          No Additives
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"healthy"}
+          iconName={"NoAlergens"}
+          name={"No Alergens"}
+          id={"114123"}
+        >
+          No Alergens
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"healthy"}
+          iconName={"NoSugar"}
+          name={"No Sugar"}
+          id={"112312323"}
+        >
+          No Sugar
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Omega3"} name={"Omega 3"} id={"11823"}>
+          Has Healthy Fats: Omega 3
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Omega6"} name={"Omega 6"} id={"11723"}>
+          Has Healthy Fats: Omega 6
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Omega9"} name={"Omega 9"} id={"11623"}>
+          Has Healthy Fats: Omega 9
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"healthy"}
+          iconName={"OmegaAll"}
+          name={"Healthy Fats"}
+          id={"11523"}
+        >
+          Has Healthy Fats
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"warning"} iconName={"PalmOil"} name={"Palm Oil"} id={"1423"}>
+          Contains Palm Oil
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"warning"} iconName={"Sugar"} name={"Sugar"} id={"13223"}>
+          Has lot of Sugar
+        </MarkMenuItem>
+        <MarkMenuItem
+          onClick={handleSelect}
+          iconType={"healthy"}
+          iconName={"Proteins"}
+          name={"High Protein Content"}
+          id={"1123"}
+        >
+          High Protein content
+        </MarkMenuItem>
+        <MarkMenuItem onClick={handleSelect} iconType={"healthy"} iconName={"Vitamin"} name={"Vitamin"} id={"1223"}>
+          Has Lot of Vitamins!
+        </MarkMenuItem>
+      </MenuList>
+      <Flex flexWrap="wrap" maxW="30ch" rowGap="8px" w="100%">
+        {values.map((item) => (
+          <MarkItem
+            name={item.name}
+            iconName={item.iconName as CustomIcon}
+            type={item.type}
+            id={item.id}
+            key={item.id}
+            onClick={handleRemoveClick}
+          ></MarkItem>
+        ))}
+      </Flex>
+    </Menu>
+  );
+};
+
+type MarkItemProps = {
+  type: "healthy" | "warning" | "dangerous";
+  iconName: CustomIcon;
+  name: string;
+  onClick: (e: SyntheticEvent, id: string) => void;
+  id: string;
+};
+
+const MarkItem = ({ type, name, iconName, id, onClick }: MarkItemProps) => {
+  const [color, setColor] = useState("gray.900");
+  const [bgColor, setBgColor] = useState("gray.900");
+  const [showButton, setShowButton] = useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    setShowButton(true);
+  };
+  const handleMouseLeave = () => {
+    setShowButton(false);
+  };
+
+  useEffect(() => {
+    switch (type) {
+      case "dangerous":
+        setColor("red.700");
+        setBgColor("red.300");
+        break;
+      case "healthy":
+        setColor("green.700");
+        setBgColor("green.300");
+        break;
+      case "warning":
+        setColor("yellow.700");
+        setBgColor("yellow.300");
+        break;
+    }
+  }, [type]);
+  return (
+    <Box
+      flexBasis="32px"
+      w="32px"
+      h="32px"
+      bg={bgColor}
+      borderRadius="full"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      onClick={(e) => onClick(e, id)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      mr="12px"
+      cursor="pointer"
+      position="relative"
+    >
+      <IconTooltip color={color} w="24px" h="24px" as={getIcon(iconName)}>
+        {name}
+      </IconTooltip>
+      <IconButton
+        hidden={!showButton}
+        position="absolute"
+        zIndex="2"
+        minW="20px"
+        maxW="20px"
+        minH="20px"
+        maxH="20px"
+        aria-label="Remove Mark"
+        pointerEvents={"none"}
+        icon={<MdClose color={"white"} size="16px" />}
+        bg="red.600"
+        _active={{ bg: "blackAlpha.700" }}
+        borderRadius="full"
+        size="xs"
+      />
+      <VisuallyHidden>
+        <IconButton aria-label="Remove Mark" icon={<MdClose />} />
+      </VisuallyHidden>
+    </Box>
   );
 };
 
