f1fae57fb2614f9a05d5e7910b1c3e62aa4542e6
Marek Fodor <fodormarek5@gmail.com>
2022-08-01 12:56:17 +0200

    Created Select and Remove factory functions

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -9,3 +9,31 @@ export function handleRemoveFactoryFunction<T extends { id: string }>(array: T[]
     setter(array.filter((item) => item.id !== id));
   };
 }
+
+export function handleSelectFactoryFunction<T extends { id: string }>(
+  data: T[],
+  array: T[],
+  setter: (arg: T[]) => void
+) {
+  return (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (e.type === "click") {
+      const event = e as SyntheticEvent<HTMLButtonElement>;
+      event.preventDefault();
+      const value = event.currentTarget.dataset.id;
+      if (array!.some((item) => item.id === value)) {
+        return;
+      }
+      setter!([{ ...data.filter((item) => item.id === value)[0] }, ...array]);
+    } else {
+      const event = e as SyntheticEvent<HTMLSelectElement>;
+      const value = event.currentTarget.options[event.currentTarget.selectedIndex].dataset.id;
+      if (event.currentTarget.selectedIndex === 0 || (array && array.some((item) => item.id === value))) {
+        event.currentTarget.selectedIndex = 0;
+        return;
+      }
+      event.currentTarget.selectedIndex = 0;
+      setter!([{ ...data.filter((item) => item.id === value)[0] }, ...array]);
+    }
+  };
+}
