b32cf6208397ac9cd8b01356ad762b011ce6ad6a
Marek Fodor <fodormarek5@gmail.com>
2022-08-02 17:59:33 +0200

    exported schema instead of model

--- Backend/Model Schemas/Meal/Ingredient.ts
+++ Backend/Model Schemas/Meal/Ingredient.ts
@@ -1,8 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-const IngredientSchema: Schema = new Schema({
+export const IngredientSchema: Schema = new Schema({
   productID: mongoose.Types.ObjectId,
   weight: Number,
 });
-
-export default mongoose.model("Ingredient", IngredientSchema);
