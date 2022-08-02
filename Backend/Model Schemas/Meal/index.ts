efe7c3944d8bb209fd68d14aa6ed0033eba47d03
Marek Fodor <fodormarek5@gmail.com>
2022-08-02 17:59:42 +0200

    Created meal schema

--- Backend/Model Schemas/Meal/index.ts
+++ Backend/Model Schemas/Meal/index.ts
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from "mongoose";
+import { IngredientSchema } from "./Ingredient";
 
-const MealSchema: Schema = new Schema({});
+const MealSchema: Schema = new Schema({
+  name: String,
+  brandID: mongoose.Types.ObjectId,
+  packingWeight: Number,
+  markIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Mark" }],
+  foodAdditiveIDs: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "FoodAdditive" },
+  ],
+  alergenIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Alergen" }],
+  categoryIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
+  ingredients: IngredientSchema,
+});
 
 export default mongoose.model("Meal", MealSchema);
