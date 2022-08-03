8a5f4838bd665059dca0eb7e6bdfa254656e65e2
Marek Fodor <fodormarek5@gmail.com>
2022-08-03 13:29:07 +0200

    created schema for user

---
+++ Backend/Model Schemas/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  profilePicture: Buffer | null;
+  roles: string[];
+  _doc: Omit<this, "_doc">;
+}
+
+const UserSchema = new mongoose.Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: mongoose.Schema.Types.Buffer, default: null },
+    roles: [{ type: String }],
+  },
+  { timestamps: true }
+);
+
+export type UserType = InferSchemaType<typeof UserSchema>;
+
+export default mongoose.model("User", UserSchema);
