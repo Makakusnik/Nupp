e06fef8d76164369694724ae5bd7ef9231c6b880
Marek Fodor <fodormarek5@gmail.com>
2022-08-04 09:10:15 +0200

    Added update, refresh, logout funcs & endpoints

--- Backend/routes/auth/functions.ts
+++ Backend/routes/auth/functions.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import User from "../../Model Schemas/User.js";
+import jwt, { VerifyOptions } from "jsonwebtoken";
+import User, { UserType } from "../../Model Schemas/User.js";
 import CryptoJS from "crypto-js";
+import { Error } from "mongoose";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../../utils/index.js";
+
+let refreshTokens: string[] = [];
 
 export const login = async (req: Request, res: Response) => {
   try {
@@ -19,15 +26,13 @@ export const login = async (req: Request, res: Response) => {
       return;
     }
 
-    const accessToken = jwt.sign(
-      { id: user!._id, roles: user!.roles },
-      process.env.SECRET_KEY!,
-      { expiresIn: "5d" }
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
 
     const { password, ...info } = user!._doc;
 
-    res.status(200).json({ ...info, accessToken });
+    res.status(200).json({ ...info, accessToken, refreshToken });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -49,4 +54,84 @@ export const register = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json(err);
   }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const currentUserID = req.user?.id;
+    const userToEditID = req.params.id;
+
+    if (req.user?.roles.includes("admin") || currentUserID === userToEditID) {
+      User.findByIdAndUpdate(
+        userToEditID,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+          },
+        },
+        { returonOriginal: true }
+      )
+        .then(() => {
+          if (req.body.username || req.body.email)
+            res.status(200).json("User updated!");
+          else res.status(200).json("Theres nothing to update!");
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    } else {
+      res
+        .status(500)
+        .json("You are not authenticated to update this user's info.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY!
+    ).toString(),
+    roles: req.body.roles,
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json({ user, message: "Successfully added." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const refresh = async (req: Request, res: Response) => {
+  const refreshToken = req.body.token;
+  //send error if there is no token or it's invalid
+  if (!refreshToken) return res.status(401).json("You are not authenticated!");
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("Refresh token is not valid!");
+  }
+
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET_KEY!);
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+  const newAccessToken = generateAccessToken(decoded as UserType);
+  const newRefreshToken = generateRefreshToken(decoded as UserType);
+
+  refreshTokens.push(newRefreshToken);
+
+  res.status(200).json({
+    accessToken: newAccessToken,
+    refreshToken: newRefreshToken,
+  });
+};
+
+export const logout = async (req: Request, res: Response) => {
+  const refreshToken = req.body.token;
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.status(200).json("You logged out successfully.");
 };
--- Backend/routes/auth/index.ts
+++ Backend/routes/auth/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, register } from "./functions.js";
+import verify from "../../utils/index.js";
+import { login, logout, refresh, register, updateUser } from "./functions.js";
 
 const router = express.Router();
 
@@ -8,5 +9,14 @@ router.post("/register", register);
 
 //LOGIN
 router.post("/login", login);
+
+//UPDATE
+router.put("/update/:id", verify, updateUser);
+
+//REFRESH
+router.post("/refresh", verify, refresh);
+
+//LOGOUT
+router.post("/logout", verify, logout);
 
 export default router;
