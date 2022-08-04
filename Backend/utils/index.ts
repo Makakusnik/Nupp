37f9275ced5e2dbfdfcc278932c149afae5ae2f0
Marek Fodor <fodormarek5@gmail.com>
2022-08-04 09:10:34 +0200

    Added token generator funcitions

--- Backend/utils/index.ts
+++ Backend/utils/index.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { UserType } from "../Model Schemas/User";
 
 function verify(req: Request, res: Response, next: () => void) {
   const authHeader: string = req.headers.token as string;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
+
     jwt.verify(token, process.env.SECRET_KEY as jwt.Secret, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
+      if (err) {
+        res.status(403).json("Token is not valid!");
+      }
+      if (typeof req.user !== "string") {
+        req.user = user as jwt.JwtPayload;
+      }
       next();
     });
   } else {
@@ -16,3 +22,20 @@ function verify(req: Request, res: Response, next: () => void) {
 }
 
 export default verify;
+
+export const generateAccessToken = (user: UserType) => {
+  return jwt.sign(
+    { id: user!.id, roles: user!.roles },
+    process.env.SECRET_KEY!,
+    {
+      expiresIn: "15m",
+    }
+  );
+};
+
+export const generateRefreshToken = (user: UserType) => {
+  return jwt.sign(
+    { id: user!.id, roles: user!.roles },
+    process.env.REFRESH_SECRET_KEY!
+  );
+};
